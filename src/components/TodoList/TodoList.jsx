644785ad41a404b700a458a6c286ffbc04f8c9e5
eunbi-kang <ebzzang1905@naver.com>
2024-04-25 11:42:40 +0900

    localStorge 사용하여 Page Refresh 됐을 때  List Item 데이터 간직하도록

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
-import styles from './TodoList.module.css'
+import styles from "./TodoList.module.css";
 
-export default function TodoList({filter}) {
-  const [todos, setTodos] = useState([
-    { id: "123", text: "호야와 장보기", status: "active" },
-    { id: "124", text: "먼비와 공부하기", status: "active" },
-  ]);
+export default function TodoList({ filter }) {
+  const [todos, setTodos] = useState(readTodosFromLocalStorage());
+
   //todos를 풀어서 새로 추가 된 것만 업데이트 해 줌
   const handleAdd = (todo) => setTodos([...todos, todo]);
   // 업데이트 하고자 하는 것만 업데이트 하는 걸로 바꿔주고 기존의 아이템을 그대로 유지하면서 새로운 배열을 만듦
-  const handleUpdate = (updated) => setTodos(todos.map(t => t.id === updated.id ? updated : t));
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
   // 아이디가 삭제하고자 하는 아이디가 아닌 것들만 모아서 setTodos 배열에 모아줌
-  const handleDelete = (deleted) => setTodos(todos.filter(t=> t.id !== deleted.id));
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((t) => t.id !== deleted.id));
 
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   const filtered = getFilteredItems(todos, filter);
   return (
     <section className={styles.container}>
@@ -33,9 +36,15 @@ export default function TodoList({filter}) {
   );
 }
 
-function getFilteredItems(todos, filter){
-  if(filter === 'all'){
+function readTodosFromLocalStorage() {
+  console.log('readTodosFromLocalStorage');
+  const todos = localStorage.getItem('todos');
+  return todos ? JSON.parse(todos) : []  ;
+}
+
+function getFilteredItems(todos, filter) {
+  if (filter === "all") {
     return todos;
   }
-  return todos.filter(todo => todo.status === filter);
+  return todos.filter((todo) => todo.status === filter);
 }
